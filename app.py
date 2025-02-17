cccba500c7d55c12deb81e5158af309ff06cc6b4
endorgobio <endorgobio@gmail.com>
2025-02-16 21:37:20 -0500

    New Graph
    version with interactivity and  reading data from user

--- app.py
+++ app.py
@@ -1,36 +1,56 @@
-from flask import Flask, render_template, jsonify
-import setuptools.dist
+from flask import Flask, render_template, request, jsonify
+import plotly.graph_objs as go
+import plotly.io as pio
+import pandas as pd
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Sample datasets
-datasets = {
-    "dataset1": {
-        "x": [1, 2, 3, 4, 5],
-        "y": [10, 15, 13, 17, 21],
-        "z": [5, 10, 7, 12, 15]
-    },
-    "dataset2": {
-        "x": [1, 2, 3, 4, 5],
-        "y": [5, 10, 7, 12, 15],
-        "z": [10, 15, 13, 17, 21]
-    },
-    "dataset3": {
-        "x": [1, 2, 3, 4, 5],
-        "y": [20, 18, 22, 19, 25],
-        "z": [15, 12, 18, 14, 20]
-    }
+data = {
+    'Dataset 1': {'x': [1, 2, 3, 4, 5], 'y': [10, 15, 7, 12, 17]},
+    'Dataset 2': {'x': [1, 2, 3, 4, 5], 'y': [5, 10, 15, 10, 5]},
+    'Dataset 3': {'x': [1, 2, 3, 4, 5], 'y': [3, 6, 9, 12, 15]}
 }
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+def create_plot(df):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.iloc[:, 0], y=df.iloc[:, 1], mode='lines+markers', name='User Data'))
+    fig.update_layout(title='Plotly Line Graph', xaxis_title='X Axis', yaxis_title='Y Axis')
+    return pio.to_json(fig)
 
-@app.route('/get_data/<dataset_id>')
-def get_data(dataset_id):
-    # Return the selected dataset as JSON
-    data = datasets.get(dataset_id, {"x": [], "y": [], "z": []})
-    return jsonify(data)
+def create_plot_from_dataset(dataset_name):
+    df = pd.DataFrame(data[dataset_name])
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['x'], y=df['y'], mode='lines+markers', name=dataset_name))
+    fig.update_layout(title='Plotly Line Graph', xaxis_title='X Axis', yaxis_title='Y Axis')
+    return pio.to_json(fig)
+
+@app.route('/')
+def index():
+    return render_template('index.html', datasets=list(data.keys()))
+
+@app.route('/update_graph', methods=['POST'])
+def update_graph():
+    dataset_name = request.json.get('dataset')
+    graph_json = create_plot_from_dataset(dataset_name)
+    return jsonify(graph_json)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        df = pd.read_json(filepath)
+        graph_json = create_plot(df)
+        return jsonify(graph_json)
 
 if __name__ == '__main__':
     app.run(debug=True)
